c22b328bb88ffd3a0b9c77d96802d65098a1acf3
Saad Edroos <saadedroossaad@gmail.com>
2025-03-01 10:14:15 +0400

    Remove banner

--- data/heroslides.js
+++ data/heroslides.js
@@ -236,16 +236,16 @@ export const slidesData2 = [
   },
 ];
 export const slidesData3 = [
-  {
-    id: 1,
-    imageSrc: "/assets/images/banner/National-Day.jpg",
-    season: "",
-    objectPosition: "50% center",
-    title: "",
-    subtitle: "",
-    href: "shop/",
-    color: "text-black"
-  },
+  // {
+  //   id: 1,
+  //   imageSrc: "/assets/images/banner/National-Day.jpg",
+  //   season: "",
+  //   objectPosition: "50% center",
+  //   title: "",
+  //   subtitle: "",
+  //   href: "shop/",
+  //   color: "text-black"
+  // },
   {
     id: 2,
     imageSrc: "/assets/images/home/la-rose.jpg",
@@ -269,16 +269,16 @@ export const slidesData3 = [
   
 ];
 export const slidesData33 = [
-  {
-    id: 1,
-    imageSrc: "/assets/images/banner/Mobile-Extend.jpg",
-    season: "",
-    objectPosition: "50% center",
-    title: "",
-    subtitle: "",
-    href: "shop/",
-    color: "text-black"
-  },
+  // {
+  //   id: 1,
+  //   imageSrc: "/assets/images/banner/Mobile-Extend.jpg",
+  //   season: "",
+  //   objectPosition: "50% center",
+  //   title: "",
+  //   subtitle: "",
+  //   href: "shop/",
+  //   color: "text-black"
+  // },
   {
     id: 2,
     imageSrc: "/assets/images/home/la-rose.jpg",
