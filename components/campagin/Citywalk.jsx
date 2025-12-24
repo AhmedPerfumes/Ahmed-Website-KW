08be98b6fe426ac4073b11bdbf7e69c04a081460
saood ghare <saoodghare0809@gmail.com>
2025-12-24 10:47:23 +0400

    Update DiscountGrid title to use dynamic value from saleDesktop

--- components/campagin/Citywalk.jsx
+++ components/campagin/Citywalk.jsx
@@ -150,7 +150,7 @@ function Sale() {
             </div>
         <div className="mt-3">
 
-        <DiscountGrid title="Winter Luxury, Elevated Offers" onlyDiscounted={true}/>
+        <DiscountGrid title={saleDesktop?.title} onlyDiscounted={true}/>
         </div>
             {/* <section className="d-flex section-3">
                 <div className="">
