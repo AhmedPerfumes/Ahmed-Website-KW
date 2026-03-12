85a634bbd8ae04bcbe41532d10e7649c36b539f6
Yazil Ansari <yazilansari@gmail.com>
2026-03-12 13:00:54 +0400

    remove .env from  tracking

--- components/shopCartandCheckout/OrderPaymentCompleted.jsx
+++ components/shopCartandCheckout/OrderPaymentCompleted.jsx
@@ -39,13 +39,13 @@ export default function OrderPaymentCompleted({ orderDetails }) {
         console.log('else if');
         return <td>{((elm.price - (elm.price / 100 * elm.coupon.value)) * elm.quantity).toFixed(currency.decimals)}{ currency.symbol }</td>;
     } else if(elm?.sale_price) {
-        return <td>{(((elm.price * 1.05) - (elm.sale_price)) * elm.qty).toFixed(currency.decimals)}{ currency.symbol }</td>;
+        return <td>{(((elm.price * 1) - (elm.sale_price)) * elm.qty).toFixed(currency.decimals)}{ currency.symbol }</td>;
     } else {
         console.log('else');
         if(elm?.product_category && elm.product_category == 'Collections') {
           return <td>{ elm.gross_amount }{ currency.symbol }</td>;
         }
-        return <td>{((elm.price * 1.05) * elm.qty).toFixed(2)}{ currency.symbol }</td>;
+        return <td>{((elm.price * 1) * elm.qty).toFixed(2)}{ currency.symbol }</td>;
     }
   };
 
