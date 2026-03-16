59a3f55f3f93590e1fb5cecd5679cb4a2ea721ee
Yazil Ansari <yazilansari@gmail.com>
2026-03-16 11:37:46 +0400

    Added Origin in Header

--- app/[locale]/(shoplist)/product-category/[category]/[subcategory]/page.jsx
+++ app/[locale]/(shoplist)/product-category/[category]/[subcategory]/page.jsx
@@ -11,6 +11,7 @@ import RelatedSlider from "@/components/singleProduct/RelatedSlider";
 // import Link from "next/link";
 import QuickView from "@/components/modals/QuickView";
 import CollapsibleDescription from "@/components/shoplist/CollapsibleDescription";
+import { headers } from 'next/headers';
 
 // export const metadata = {
 //   title: "Perfumes | Buy Best Perfumes Online | Ahmed Perfume",
@@ -20,7 +21,21 @@ import CollapsibleDescription from "@/components/shoplist/CollapsibleDescription
 //   },
 // };
 
+function getRequestOrigin() {
+  const headersList = headers();
+  const host = headersList.get('host') || process.env.NEXT_PUBLIC_DEFAULT_ORIGIN; // e.g., 'localhost:3000' or 'yourdomain.com'
+  const protocol = headersList.get('x-forwarded-proto') || 'https'; // or 'https'
+  
+  // if (!host) {
+  //   // Fallback for local development or edge cases
+  //   return process.env.NEXT_PUBLIC_DEFAULT_ORIGIN || 'http://localhost:3000';
+  // }
+
+  return `${protocol}://${host}`;
+}
+
 async function getCategorySubCategory(categoryName, subCategoryName) {
+  const origin = getRequestOrigin();
   const catSlug = categoryName.toLowerCase();
   const subSlug = subCategoryName.toLowerCase();
   // console.log(`${process.env.NEXT_PUBLIC_API_URL}api/products?category=${categoryName.split("-").join(" ").toUpperCase()}&subCategory=${subCategoryName.split("-").join(" ").toUpperCase()}`);
@@ -28,6 +43,7 @@ async function getCategorySubCategory(categoryName, subCategoryName) {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
+      'origin': origin,
     },
     body: JSON.stringify({
       category: categoryName.split("-").join(" ").toUpperCase(),
@@ -57,12 +73,14 @@ async function getProductCategorySEO(categoryName, subCategoryName) {
   //     product: product.split("-").join(" ").toUpperCase(),
   //   })
   // });
+  const origin = getRequestOrigin();
   const response = await fetch(
       `${process.env.NEXT_PUBLIC_API_URL}api/productCategorySEO`,
       {
           method: "POST",
           headers: {
               "Content-Type": "application/json",
+              'origin': origin,
           },
           body: JSON.stringify({
               category: categoryName.split("-").join(" ").toUpperCase(),
